f2792aafecf5c014e85e8ad97cee8c1c31140635
Iain Collins <me@iaincollins.com>
2013-12-01 06:29:17 +0000

    Improved error handling and robustness of parser/fetcher (improved promise chain behaviour) and now more cleanly handles errors and DB disconnection.
    Should show more detailed errors and cope with a wider range of states - such as when bills have a documents page but no bill text yet (which can happen when there are explanatory documents or related reports), which wasn't previously cleanly accounted for.

--- scripts/get-bills.js
+++ scripts/get-bills.js
@@ -1,5 +1,14 @@
 #!/usr/bin/env node
 
+/**
+ * Get the latest bills from the parilament website,
+ * related events from TheyWorkForYou and other info
+ * related to bills (such as the sponsors).
+ *
+ * @todo Fetch details about the member if not found already (party, constituency, photo, etc)
+ * @todo Extract the full text of the bill by screen scraping.
+ */
+
 var util = require('util'); // For debugging
 var crypto = require('crypto');
 var mongoJs = require('mongojs');
@@ -11,27 +20,38 @@ var billParser = require(__dirname + '/../lib/billparser');
 
 var date = new Date();
 
-console.log("Updating Public Scrutiny Office database...");
+console.log("*** Updating the Public Scrutiny Office database");
 
 var databaseUrl = "127.0.0.1/public-scrutiny-office";
 var collections = ["bills", "members", "events"];
 var db = mongoJs.connect(databaseUrl, collections);
 // db.bills.drop();
-//db.events.drop();
+// db.events.drop();
 
 // Get bill details (involves several lookups, hence promises)
 // @fixme close DB connection when all bills have been added.
+var bills;
 return getBills()
 .then(function(bills) {    
-    return bills.forEach(function(bill, index) {
-         // return billParser.getBillDetails(bill)
-         // .then(function(bill) {
-         //     // Add bill to database
-         //     addBill(bill);
-         //     getEventsForBill(bill);
-         // });         
-        getEventsForBill(bill);
+    var promises = [];
+    bills.forEach(function(bill, index) {
+        var promise = billParser.getBillDetails(bill);
+        promises.push(promise);
     });
+  return Q.all(promises);
+})
+.then(function(bills) {
+    var promises = [];
+    bills.forEach(function(bill, index) {
+        // Look for events for related to each bill
+        var promise = getEventsForBill(bill);
+        promises.push(promise);
+    });
+    return Q.all(promises);
+})
+.then(function() {
+    console.log("*** Finished updating the Public Scrutiny Office database");
+    db.close();
 });
 
 function getBills() {
@@ -44,7 +64,8 @@ function getBills() {
     
         // Check the response seems okay
         if (response.statusCode != 200) {
-            console.log("Unable to fetch list of bills before parliament from services.parliament.uk");
+            console.log("*** Unable to fetch list of bills before parliament from services.parliament.uk");
+            console.log(response);
             return;
         }
 
@@ -54,8 +75,8 @@ function getBills() {
             console.log("Found "+result.rss.channel[0].item.length+" bills");
             for (i=0; i<result.rss.channel[0].item.length; i++) {
                 var item = result.rss.channel[0].item[i];
-            
-                var bill = {};                
+
+                var bill = {};
                 // Accessing the GUID value is kind of funky.
                 // As it's actually a URL, we make our own from an SHA1 hash of the strong.
                 bill.id = crypto.createHash('sha1').update( item.guid[0]._ ).digest("hex");
@@ -71,13 +92,12 @@ function getBills() {
         });
         
         deferred.resolve(bills);
-        
     });
-    
     return deferred.promise;
 }
 
 function addBill(bill) {    
+    var deferred = Q.defer();
     db.bills.save({
         _id: bill.id,
         year: bill.year,
@@ -90,63 +110,83 @@ function addBill(bill) {
         } else {
             console.log("Bill added: "+bill.name);
         }
+        deferred.resolve(bill);
     });
+    return deferred.promise;
 }
 
 function getEventsForBill(bill) {
+    var deferred = Q.defer();
+    var promises = [];
     
     // Get events matching the keywords string from TheyWorkForYou
     request('http://www.theyworkforyou.com/api/getHansard?key=GfmMVnCm29fQEqvFS7CgLHLJ&search='+encodeURIComponent(bill.name)+'&output=js',
             function (error, response, body) {
 
         // Check the response seems okay
-        if (response.statusCode != 200)
+        if (response.statusCode != 200) {
+            console.log("*** Invalid HTTP status response (unable to fetch events for bill)");
+            console.log(response);
             return;
+        }
 
-        var events = JSON.parse(body);
-        for (i=0; i<events.rows.length; i++) {
-            var row = events.rows[i];
+        var jsonResponse = JSON.parse(body);
+        for (i=0; i<jsonResponse.rows.length; i++) {
+            var row = jsonResponse.rows[i];
 
             // Ignore events that don't have all of these properties
             // (they are not relevant to bills)
             if (!row.title || !row.event_date || !row.link_external)
                 continue;
 
-            var event = {};                
-            // Accessing the GUID value is kind of funky.
-            // As it's actually a URL, we make our own from an SHA1 hash of the strong.
-            event.id = crypto.createHash('sha1').update( row.event_date + row.title ).digest("hex");
-            // Special case handling to create more readable event names
-            event.name = row.title;                
-            if (event.name == "to consider the Bill")
-                event.name = "Consideration of the "+bill.name+" Bill";
-            if (event.name == bill.name)
-                event.name = bill.name+" Bill";
+            var event = {};
+            try {
+                var event = {};
+                // Accessing the GUID value is kind of funky.
+                // As it's actually a URL, we make our own from an SHA1 hash of the strong.
+                event.id = crypto.createHash('sha1').update( row.event_date + row.title ).digest("hex");
+                // Special case handling to create more readable event names
+                event.name = row.title;                
+                if (event.name == "to consider the Bill")
+                    event.name = "Consideration of the "+bill.name+" Bill";
+                if (event.name == bill.name)
+                    event.name = bill.name+" Bill";
             
-            // Some of the event names end in " - /" (with varyign amounts of spaces)
-            // This is likely a  result of them being screenscraped by the 
-            // TheyWorkForYouAPI (the Parliament website displays events this way)
-            event.name = event.name.replace(/(\s*)-(\s*)\/$/g, '');
+                // Some of the event names end in " - /" (with varyign amounts of spaces)
+                // This is likely a  result of them being screenscraped by the 
+                // TheyWorkForYouAPI (the Parliament website displays events this way)
+                event.name = event.name.replace(/(\s*)-(\s*)\/$/g, '');
             
-            event.name = phpjs.trim(event.name);
+                event.name = phpjs.trim(event.name);
             
-            event.url = row.link_external;
-            event.date = row.event_date;
-        
-            // Not logging the full bill object, just what's needed.
-            event.bill = {};
-            event.bill.id = bill.id;
-            event.bill.name = bill.name;
-            event.bill.url = bill.url;
-            
-            // Add event to database
-            addEvent(event);
-        }
-    });
+                event.url = row.link_external;
+                event.date = row.event_date;
 
+                // Not logging the full bill object, just what's needed.
+                event.bill = {};
+                event.bill.id = bill.id;
+                event.bill.name = bill.name;
+                event.bill.url = bill.url;
+
+            } catch (exception) {
+                // Ignore exceptions
+            }
+            
+            var promise = addEvent(event);
+            promises.push( promise );
+        }
+        deferred.resolve( promises );
+    });
+    return deferred.promise;
 }
 
-function addEvent(event) {    
+function addEvent(event) {  
+    var deferred = Q.defer();
+    
+    // Don't add blank events
+    if (!event.name)
+        return;
+        
     db.events.save({
         _id: event.id,
         name: event.name,
@@ -159,8 +199,7 @@ function addEvent(event) {
         } else {
             console.log("Event added: "+event.name);
         }
+        deferred.resolve(event);
     });
+    return deferred.promise;
 }
-
-// @todo Fetch details about the member if not found already (party, constituency, photo, etc)
-// @todo Extract the full text of the bill by screen scraping.
