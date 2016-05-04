e73fb09976c9713ddc64af41be589d1f7e5897d5
Iain Collins <me@iaincollins.com>
2016-05-04 19:54:54 +0100

    Adding annodation support back!
    annotateit.org has some serious problems so switched to hypothes.is which works and is quite low friction.

---
+++ scripts/update-members.js
@@ -0,0 +1,12 @@
+
+// Create $timestamp from current date+time
+
+// Get all members in the Commons from Parliament API
+// Add $timestamp to record as .lastUpdated property
+// Save using Parliament ID as the object key.
+
+// Get all members in the Lords from Parliament API
+// Add $timestamp to record as .lastUpdated property
+// Save using Parliament ID as the object key.
+
+// Set all Members .active property to false if .lastUpdated != $timestamp
